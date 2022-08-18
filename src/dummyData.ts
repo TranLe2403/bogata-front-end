8efed83c834dab622a39ab9def7609171c375f4b
TranLe2403 <trantran.0324@gmnail.com>
2022-08-18 23:43:42 +0300

    add dummy game data

---
+++ src/dummyData.ts
@@ -0,0 +1,71 @@
+import { GameItem } from './App';
+
+export const gameItemsData: GameItem[] = [
+  {
+    id: '123cfgdf',
+    name: 'Zombicide Season 3: Rue Morgue',
+    minPlayer: 1,
+    maxPlayer: 12,
+    playDuration: 60,
+    available: true,
+    dateAdded: '2022/08/18',
+    gameSize: 'normal',
+    minAge: 14,
+    rating: 7.5,
+    genre: ['Action', 'Co-op']
+  },
+  {
+    id: 'dfd232ef',
+    name: 'Welcome To',
+    minPlayer: 1,
+    maxPlayer: 100,
+    playDuration: 25,
+    available: true,
+    dateAdded: '2022/08/18',
+    gameSize: 'small',
+    minAge: 10,
+    rating: 7.6,
+    genre: ['City Building']
+  },
+  {
+    id: '23r78hnh',
+    name: 'Splendor',
+    minPlayer: 2,
+    maxPlayer: 4,
+    playDuration: 30,
+    available: true,
+    dateAdded: '2022/08/18',
+    gameSize: 'normal',
+    minAge: 10,
+    rating: 7.4,
+    genre: ['Trading']
+  },
+  {
+    id: '90876tg4',
+    name: 'Werewolves: The of Mole Valley',
+    minPlayer: 8,
+    maxPlayer: 18,
+    playDuration: 30,
+    available: false,
+    dateAdded: '2022/08/18',
+    gameSize: 'small',
+    minAge: 10,
+    rating: 6.5,
+    genre: ['RPG']
+  },
+  {
+    id: '6754rgtf',
+    name: 'Stop Thief',
+    description: 'installing game application is needed',
+    minPlayer: 1,
+    maxPlayer: 4,
+    playDuration: 45,
+    available: false,
+    dateAdded: '2022/08/18',
+    gameSize: 'normal',
+    minAge: 8,
+    rating: 7.0,
+    genre: ['Detective']
+  },
+];
+
