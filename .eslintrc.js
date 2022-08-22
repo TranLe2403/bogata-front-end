fa6a2035550fb1bf3b3c026642703df5841fbcab
TranLe2403 <trantran.0324@gmnail.com>
2022-08-22 23:25:13 +0300

    add some eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,12 +11,31 @@ module.exports = {
     'eslint:recommended',
     'plugin:prettier/recommended'
   ],
-  overrides: [],
+  parser: '@typescript-eslint/parser',
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+
+      // As mentioned in the comments, you should extend TypeScript plugins here,
+      // instead of extending them outside the `overrides`.
+      // If you don't want to extend any rules, you don't need an `extends` attribute.
+      // extends: [
+      //   'plugin:@typescript-eslint/recommended',
+      //   'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      // ],
+
+      parserOptions: {
+        project: ['./tsconfig.json'] // Specify it only for TypeScript files
+      }
+    }
+  ],
   parserOptions: {
     ecmaVersion: 'latest'
   },
   plugins: ['react'],
   rules: {
-    'react/react-in-jsx-scope': 'off'
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'prettier/prettier': 'off'
   }
 };
