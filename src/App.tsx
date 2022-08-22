9f1e826173f5ba9f8e800167f66a1d63e9e4c765
TranLe2403 <trantran.0324@gmnail.com>
2022-08-22 23:28:59 +0300

    take use of GameItem component and some updates in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import './App.css';
+import GameItem from './components/GameItem';
+import { gameItemsData } from './dummyData';
+import styled from '@emotion/styled';
 
-export type GameItem = {
+export interface GameItemType {
   id: string;
   name: string;
   description?: string;
@@ -11,16 +14,51 @@ export type GameItem = {
   available: boolean;
   dateAdded: string;
   gameSize: 'small' | 'normal' | 'large';
-  genre: Genre[],
+  genre: Genre[];
   minAge: number;
   rule?: string;
   rating: number;
-};
-
-type Genre = 'Co-op' | 'Action' | 'Survival' | 'Trading' | 'Strategy' | 'Resource Management' | 'RPG' | 'City Building' | 'Detective' // Consider to use enum type
-
-function App() {
-  return <div className="App">Welcome to Bogata</div>;
 }
+
+const AppStyle = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const GameListStyle = styled.div`
+  padding: 32px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`
+
+type Genre =
+  | 'Co-op'
+  | 'Action'
+  | 'Survival'
+  | 'Trading'
+  | 'Strategy'
+  | 'Resource Management'
+  | 'RPG'
+  | 'City Building'
+  | 'Detective'; // Consider to use enum type
+
+const App = () => {
+  return (
+    <AppStyle>
+      <GameListStyle>
+        {gameItemsData
+          .filter((game) => game.available)
+          .map((item) => (
+            <GameItem key={item.id} data={item} />
+          ))}
+      </GameListStyle>
+    </AppStyle>
+  );
+};
 
 export default App;
