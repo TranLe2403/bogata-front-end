528612b893041b74bf1456e501877b666904e14c
TranLe2403 <trantran.0324@gmnail.com>
2022-09-01 01:02:52 +0300

    Add google auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import jwtDecode from 'jwt-decode';
 import styled from '@emotion/styled';
 
 import './App.css';
@@ -23,6 +24,25 @@ export interface GameItemType {
   rating: number;
 }
 
+interface UserInfo {
+  email: string;
+  name: string;
+  picture: string;
+}
+
+type Genre =
+  | 'Co-op'
+  | 'Action'
+  | 'Survival'
+  | 'Trading'
+  | 'Strategy'
+  | 'Resource Management'
+  | 'RPG'
+  | 'City Building'
+  | 'Detective'; // Consider to use enum type
+
+type EventType = React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLDivElement, MouseEvent>;
+
 const AppStyle = styled.div`
   width: 100%;
   height: 100%;
@@ -39,31 +59,52 @@ const GameListStyle = styled.div`
   gap: 16px;
 `;
 
-type Genre =
-  | 'Co-op'
-  | 'Action'
-  | 'Survival'
-  | 'Trading'
-  | 'Strategy'
-  | 'Resource Management'
-  | 'RPG'
-  | 'City Building'
-  | 'Detective'; // Consider to use enum type
-
 const App = () => {
   const [searchValue, setSearchValue] = useState<string>('');
   const [searchResult, setSearchResult] = useState<GameItemType[]>(gameItemsData);
+  const [user, setUser] = useState<UserInfo>({ email: '', name: '', picture: '' });
 
-  const searchHandler = (
-    e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) => {
+  const searchHandler = (e: EventType) => {
     e.preventDefault();
     const filtedSearchResult = gameItemsData.filter((item) => item.name.includes(searchValue));
     setSearchResult(filtedSearchResult);
   };
 
+  useEffect(() => {
+    // @ts-expect-error
+    const google = window.google;
+    google.accounts.id.initialize({
+      client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+      callback: handleCallbackResponse
+    });
+    google.accounts.id.renderButton(document.getElementById('signInDiv'), {
+      theme: 'outline',
+      size: 'large'
+    });
+  }, []);
+
+  const setSignInButtonHidden = (hidden: boolean = false) => {
+    const getElem = document.getElementById('signInDiv');
+    if (getElem === null) return;
+    getElem.hidden = hidden;
+  };
+
+  const handleCallbackResponse = (res: Record<string, string>) => {
+    const { email, name, picture }: Record<string, string> = jwtDecode(res.credential);
+    setUser({ email, name, picture });
+    setSignInButtonHidden(true);
+  };
+
+  const handleSignOut = () => {
+    setUser({ email: '', name: '', picture: '' });
+    setSignInButtonHidden();
+  };
+
   return (
     <>
+      {user.email === '' ? <div>Login</div> : <div onClick={handleSignOut}>Logout</div>}
+      <div id="signInDiv"></div>
+
       <TopBar
         setSearchValue={setSearchValue}
         searchValue={searchValue}
