5984d1f937a336e1ca7ec3541118abc0dc8c4ee9
TranLe2403 <trantran.0324@gmnail.com>
2022-10-16 19:26:39 +0300

    take use of components from MUI and rework on GameItem component

--- src/components/GameItem/index.tsx
+++ src/components/GameItem/index.tsx
@@ -1,72 +1,34 @@
 import { GameItemType } from '../../App';
-import StarIcon from '@mui/icons-material/Star';
-import styled from '@emotion/styled';
+import { Card, CardMedia, CardContent, Typography, Box } from '@mui/material';
 
-const GameItemContainer = styled.div`
-  background: #ececec;
-  padding: 16px 32px;
-  display: flex;
-  gap: 16px;
-  border-radius: 4px;
-  box-shadow: 0 0 6px rgba(0, 0, 0, 0.08);
-`;
+const getStyles: any = {
+  root: { display: 'flex', padding: 16, background: '#ECECEC' },
+  media: { height: 160, width: 160 },
+  box: { display: 'flex', justifyContent: 'space-between'}
+};
 
-const ImageStyle = styled.img`
-  width: 160px;
-  height: 160px;
-  border-radius: 4px;
-`;
-
-const NoMarginText = styled.p`
-  margin: 0;
-`;
-
-const ItalicText = styled.p`
-  font-style: italic;
-  margin: 0;
-`;
-
-const NoMarginHeading = styled.h2`
-  margin: 0;
-`;
-
-const GameInfoStyle = styled.div`
-  flex: 1;
-  display: flex;
-  gap: 8px;
-  flex-direction: column;
-`;
-
-const RatingContainer = styled.div`
-  display: flex;
-  align-items: center;
-  height: fit-content;
-  gap: 8px;
-`;
+const DEFAULT_IMG =
+  'https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png';
 
 const GameItem = ({ data }: { data: GameItemType }) => {
+  const getImageSrc = () => (data.pictures === undefined ? DEFAULT_IMG : data.pictures[0]);
+
   return (
-    <GameItemContainer>
-      <ImageStyle
-        data-testid="game-image"
-        src={
-          data.pictures !== undefined
-            ? data.pictures[0]
-            : 'https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png'
-        }
-      />
-      <GameInfoStyle>
-        <NoMarginHeading>{data.name}</NoMarginHeading>
-        <ItalicText>
+    <Card style={getStyles.root}>
+      <CardMedia style={getStyles.media} image={getImageSrc()} title="Contemplative Reptile" />
+      <CardContent sx={{ flex: 1 }}>
+        <Box sx={getStyles.box}>
+          <Typography sx={{ fontWeight: 'bold' }} gutterBottom variant="h5" component="h2">
+            {data.name}
+          </Typography>
+          <Typography component="p">BoardGameGeek rating: {data.rating}/10</Typography>
+        </Box>
+        <Typography sx={{ fontStyle: 'italic' }} component="p">
           {data.genre.map((item, i) => `${item}${i === data.genre.length - 1 ? '' : ', '}`)}
-        </ItalicText>
-        <NoMarginText>Date added: {data.dateAdded}</NoMarginText>
-      </GameInfoStyle>
-      <RatingContainer>
-        <NoMarginText>BoardGameGeek rating: {data.rating}/10</NoMarginText>
-        <StarIcon />
-      </RatingContainer>
-    </GameItemContainer>
+        </Typography>
+        <Typography component="p">Date added: {data.dateAdded}</Typography>
+      </CardContent>
+    </Card>
   );
 };
 
