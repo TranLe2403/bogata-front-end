a4f72ad8d0e7421be544224db54fc1295b76fbda
TranLe2403 <trantran.0324@gmnail.com>
2022-10-26 00:21:09 +0300

    fix linting issues

--- src/components/SearchInput/index.test.tsx
+++ src/components/SearchInput/index.test.tsx
@@ -1,20 +1,13 @@
+import { Dispatch, SetStateAction } from 'react';
 import { fireEvent, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import SeachInput from '../SearchInput';
 
 describe('<SeachInput />', () => {
-  const mockSetState: React.Dispatch<React.SetStateAction<string>> = jest.fn();
-
-  jest.mock('react', () => ({
-    useState: (initial: string) => [initial, mockSetState]
-  }));
-
-  const mockSeachHandler = jest.fn();
-
+  const mockSetState: Dispatch<SetStateAction<string>> = jest.fn();
+  
   it('should display working search bar', () => {
-    const searchInputElem = render(
-      <SeachInput setSearchValue={() => {}} />
-    );
+    const searchInputElem = render(<SeachInput setSearchValue={() => {}} />);
     const searchbarElement = searchInputElem.getByTestId('search-bar');
     const selectNode = searchbarElement.childNodes[0];
 
@@ -24,9 +17,7 @@ describe('<SeachInput />', () => {
   });
 
   it('should display search icon and it is workable', () => {
-    const searchInputElem = render(
-      <SeachInput setSearchValue={mockSetState} />
-    );
+    const searchInputElem = render(<SeachInput setSearchValue={mockSetState} />);
     const searchIconElement = searchInputElem.getByTestId('search-icon');
     const getIconElem = searchIconElement.firstChild as HTMLImageElement;
 
