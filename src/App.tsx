ed00ba2cb0ceaa86e5df622828da0220ed0657ef
TranLe2403 <trantran.0324@gmnail.com>
2025-01-23 21:04:31 +0200

    google-login

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { GameItemModal } from './components/GameItemForm';
 import { Button } from '@mui/material';
 import { FieldValues } from 'react-hook-form';
 import { getFullGameInfo } from './utils/getFullGameInfo';
+import Login from './components/Login';
+import styled from '@emotion/styled';
 
 
 export interface GameItemType {
@@ -25,6 +27,24 @@ export interface GameItemType {
   rating: number;
 }
 
+const UserDefaultImage = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+  background: red;
+  position: fixed;
+  right: 32px;
+  top: 16px;
+  z-index: 2;
+`;
+
+const UserImage = styled.img`
+  border-radius: 20px;
+  position: fixed;
+  right: 32px;
+  top: 16px;
+  z-index: 2;
+`;
 
 type Genre =
   | 'Co-op'
@@ -37,9 +57,18 @@ type Genre =
   | 'City Building'
   | 'Detective'; // Consider to use enum type
 
+  export interface UserInfo {
+    email: string;
+    name: string;
+    picture: string;
+  }
+
 export const App = (): JSX.Element => {
   const [open, setOpen] = useState(false)
   const [gameItems, setGameItems] = useState<GameItem[]>([]);
+
+  const [user, setUser] = useState<UserInfo>({ email: '', name: '', picture: '' });
+  const [showLogin, setShowLogin] = useState<boolean>(false);
 
   useEffect(() => {
     const setItems = async (): Promise<void> => {
@@ -61,12 +90,33 @@ export const App = (): JSX.Element => {
     }
   }
 
-  const handleAddGame = (): void => {
-    setOpen(true)
-  }
-  
+  const handleAddGame = (): void => setOpen(true)
+
+  const userClickHandler = (): void => setShowLogin(!showLogin);
+
   return (
     <>
+    {user.picture === '' ? (
+        <UserDefaultImage onClick={userClickHandler} />
+      ) : (
+        <UserImage
+          src={user.picture}
+          alt={user.name}
+          width={40}
+          height={40}
+          onClick={userClickHandler}
+        />
+      )}
+
+      {showLogin ? (
+        <Login
+          email={user.email}
+          showLogin={showLogin}
+          setShowLogin={setShowLogin}
+          setUser={setUser}
+        />
+      ): null}
+
       <Button onClick={handleAddGame} variant="contained">Add Game</Button>
       <GameItemModal 
         handleSubmitGame={handleSubmitGame} 
